10227adbe3cfa92408e49ff82720111315ab630f
djmittens <nick.meister37@gmail.com>
2015-06-06 17:38:18 -0600

    Beginning to debug the quicksort

--- src/main/java/sorting/Quicksort.java
+++ src/main/java/sorting/Quicksort.java
@@ -1,11 +1,14 @@
 package sorting;
 
+import util.Array;
+
+import java.awt.geom.QuadCurve2D;
 import java.util.Arrays;
 import java.util.Random;
 
 /**
  */
-public class  Quicksort <T extends Comparable<T>> {
+public abstract class  Quicksort <T extends Comparable<T>> {
 
     public static <E extends Comparable<E>> void sort(E[] list) {
         new Quicksort<E>(list).sort();
@@ -20,6 +23,7 @@ public class  Quicksort <T extends Comparable<T>> {
     }
 
     T[] sort() {
+        System.out.println("Sorting " + Arrays.toString(list));
         sort(0, list.length - 1);
         return list;
     }
@@ -36,31 +40,69 @@ public class  Quicksort <T extends Comparable<T>> {
         int pivot = pickPivot(left, right);
         // Place the pivot all the way to the right
         swap(pivot, right);
+        System.out.println("Pivot " + list[pivot]);
         // Value in the store is guranteed to be < pivot
         int store = left;
-        for(int i = left; i < right - 1; i ++) {
+        for(int i = left; i < right; i ++) {
             // Comparing each value to the pivot to see if they are greater.
-            if(list[i].compareTo(list[right]) >= 0  ) {
+            if(list[i].compareTo(list[right]) <= 0  ) {
                 swap(store, i);
                 store ++;
             }
         }
+        System.out.println(Arrays.toString(list));
         // In the end store points to last position in the subarray that is sorted.
         swap(store, right);
         return store;
     }
 
 
-    protected  int pickPivot(int left, int right) {
-       if(left >= right)
-           throw new IllegalArgumentException("Partition: Left must be less than Right");
-        // We pick the pivot randomly.
-        return (random.nextInt(right - left)) + left;
-    }
+    /**
+     * Select a pivot point within this range.
+     * @param left
+     * @param right
+     * @return
+     */
+    protected abstract int pickPivot(int left, int right);
 
-    protected void swap(int left, int right) {
+    protected final void swap(int left, int right) {
         T t = list[left];
         list[left] = list[right];
         list[right] = t;
+    }
+
+    public class RandomPivot extends Quicksort<T>{
+
+        RandomPivot(T[] list) {
+            super(list);
+        }
+
+        protected  int pickPivot(int left, int right) {
+            if(left >= right)
+                throw new IllegalArgumentException("Partition: Left must be less than Right");
+            // We pick the pivot randomly.
+            int pivot = (random.nextInt(right - left)) + left;
+            assert(pivot >= left && pivot <= right);
+            return pivot;
+        }
+    }
+
+
+    /**
+     * This implementation uses right most
+     */
+    public class NaivePivot extends Quicksort<T>{
+
+        NaivePivot(T[] list) {
+            super(list);
+        }
+
+        protected  int pickPivot(int left, int right) {
+            return right;
+        }
+    }
+
+    public class RandomIterative extends Quicksort<T> {
+        
     }
 }
--- src/test/java/sorting/QuicksortTest.java
+++ src/test/java/sorting/QuicksortTest.java
@@ -1,15 +1,29 @@
 package sorting;
 
+import org.junit.Assert;
 import org.junit.Test;
+import util.Array;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  *
  */
 public class QuicksortTest {
 
+
     @Test
     public void testSort() throws Exception {
+        Integer[] array = new Integer[10];
+        for(int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+
+        Collections.shuffle(Arrays.asList(array));
+        Quicksort.sort(array);
+        assertTrue("This sort does not sort(eg. doesn't work) " + Arrays.toString(array), Array.isSorted(array));
     }
 }
