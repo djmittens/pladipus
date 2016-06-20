b683af4715b39eb004702fb87a2f62f9ce9ddc20
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2016-06-19 17:05:56 -0700

    updating imperative spiral

--- src/main/scala/me/ngrid/hackerrank/fractals/ImperativeSpiral.scala
+++ src/main/scala/me/ngrid/hackerrank/fractals/ImperativeSpiral.scala
@@ -3,7 +3,7 @@ package me.ngrid.hackerrank.fractals
 import scala.collection.mutable
 
 /**
-  *
+  * Draws an n x n spiral by building individual segments and mutating an 2D array.
   */
 object ImperativeSpiral extends App {
   val `_` = ' '
@@ -13,35 +13,44 @@ object ImperativeSpiral extends App {
 
   case class Position(r: Int, c: Int) {
     def up(n: Int) = copy(r = r - n)
-
     def down(n: Int) = copy(r = r + n)
-
     def left(n: Int) = copy(c = c - n)
-
     def right(n: Int) = copy(c = c + n)
   }
 
-  def spiral(size: Int): Spiral = {
+  def spiral(size: Int): (Seq[Position], Spiral) = {
     val spiral = mutable.ArraySeq.fill(size, size)(`_`)
 
-    def drawSegment(n: Int, t: (Int) => Position): Option[Position] = {
-      def draw(position: Position): Option[Position] = {spiral(position.r).update(position.c, `*`); Option(position)}
+    def draw(position: Position): Unit = spiral(position.r).update(position.c, `*`)
 
-      (0 until n).foldLeft[Option[Position]](None) {(pos, x) => draw(t(x))}
+    def loop(n: Int, s: Position): Seq[Position] = {
+      def segment(n: Int, t: (Int) => Position): Option[(Seq[Position], Position)]= {
+        val o = (0 until n).map(t(_))
+        o.lastOption.map((o, _))
+      }
+
+      val a = segment(n, s.right)
+      val b = a.flatMap(x => segment(n - 1 , x._2.down(1).down))
+      val c = b.flatMap(x => segment(n - 2, x._2.left(1).left))
+      val d = c.flatMap(x => segment(n - 3, x._2.up(1).up))
+
+      Seq(a, b, c, d).filter(_.nonEmpty).flatMap(_.get._1)
     }
 
-    (size % 4).to(size, 4).foldRight[Option[Position]](Some(Position(0, 0)))((n, pos) => {
-      for {
-        start <- pos
-        p1 <- drawSegment(n, start.right)
-        p2 <- drawSegment(n, p1.down)
-        p3 <- drawSegment(n - 1, p2.left)
-        p4 <- drawSegment(n - 2, p3.up)
-      } yield p4.right(1)
+    val (segs, _) = (size % 4).to(size, 4).foldRight((Seq[Position](), Position(0, 0)))((n, r) => {
+      val (o, pos) = r
+      val a = loop(n, pos)
+      a.foreach(draw)
+
+      (o ++ a, a.lastOption.
+        map(_.right(1)).
+        getOrElse(pos))
     })
 
-    spiral
+    segs -> spiral
   }
 
-  spiral(12).foreach(x => println(x.mkString))
+  val (s, sp) = spiral(12)
+  println(s.map(Position.unapply(_).get).mkString(", "))
+  sp.foreach(x => println(x.mkString))
 }
