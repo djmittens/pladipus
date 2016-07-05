88570a44d54165460e47e10baaa8489b5fe9e734
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2016-07-05 11:20:20 -0700

    Adding problem #5 from cake, iterative still doesnt work.

---
+++ src/main/scala/me/ngrid/cake/Cashier.scala
@@ -0,0 +1,62 @@
+package me.ngrid.cake
+
+import scala.collection.mutable
+
+/**
+  *
+  */
+object Cashier {
+  def naive(amount: Int, denominations: Seq[Int]): Int = {
+    if(amount == 0 ) return 1
+    else if (amount < 0) return 0
+
+    if(denominations.isEmpty) return 0
+    println(s"naive $amount : $denominations")
+
+    denominations.zip(denominations.tails.toSeq.tail).flatMap({ z =>
+      val (x, left) = z
+      (1 to amount / x).map(y => {
+        naive(amount - (y * x), left)
+      })
+    }).sum
+  }
+
+  def memoized(amount: Int, denominations: Seq[Int]): Int = {
+    val m = mutable.HashMap[(Int, Int), Int]()
+
+    def fire(a:Int, d: Seq[Int]): Int = {
+      if(a == 0 ) return 1
+      else if (a < 0) return 0
+      if(d.isEmpty) return 0
+
+      println(s"memo $a : $d")
+
+      d.zip(d.tails.toSeq.tail).flatMap({ z =>
+        val (x, left) = z
+        (1 to a / x).map(y => {
+          val rem = a - (y * x)
+          lazy val res = fire(rem, left)
+          m.getOrElseUpdate((x, y), res)
+        })
+      }).sum
+    }
+
+    fire(amount, denominations)
+  }
+
+  def iterative(amount: Int, denoms: Seq[Int]): Int = {
+    val m = mutable.ArrayBuffer.fill[Int](amount)(0)
+    m(denoms.head) = 1
+
+    def getCount(i: Int): Int = if (i > 0) m(i) else 0
+
+    for {
+      d <- denoms
+      i <- m.indices.tail
+    } {
+      m(i) += getCount(i - d)
+    }
+
+    m.last
+  }
+}
---
+++ src/test/scala/me/ngrid/cake/Cashier$Test.scala
@@ -0,0 +1,14 @@
+package me.ngrid.cake
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+  *
+  */
+class Cashier$Test extends FlatSpec with Matchers{
+  it should "return some numbers" in {
+    Cashier.naive(4, Seq(1,2,3)) should be (4)
+    Cashier.memoized(4, Seq(1,2,3)) should be (4)
+    Cashier.iterative(4, Seq(1,2,3)) should be (4)
+  }
+}
