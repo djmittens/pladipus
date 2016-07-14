5bbfcb89a0284b79e9a33fcb4b38086780c9a32b
Nick Gridinskiy <nickgri@sparkcentral.com>
2016-07-14 12:34:01 -0700

    Adding some extra non working coding cake

---
+++ src/test/scala/me/ngrid/cake/RectangleOfLove$Test.scala
@@ -0,0 +1,9 @@
+package me.ngrid.cake
+
+import org.scalatest.{FlatSpec, FunSuite, Matchers}
+
+/**
+  */
+class RectangleOfLove$Test extends FlatSpec with Matchers{
+  it should "calculate some stuff like so "
+}
