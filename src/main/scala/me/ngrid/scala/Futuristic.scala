02c30aac0d51dc2a89896cc6cbc7cc96da24cce1
Nikita Gridinskiy <nikita.gridinskiy@gmail.com>
2016-12-08 00:12:59 -0800

    Finishing up the future dsl

--- src/main/scala/me/ngrid/scala/Futuristic.scala
+++ src/main/scala/me/ngrid/scala/Futuristic.scala
@@ -1,60 +1,10 @@
 package me.ngrid.scala
 
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success, Try}
 import scala.concurrent.duration._
+import scala.util.control.NonFatal
 
-trait MonadicFutures {
-  import scala.language.implicitConversions
-
-  implicit class MonadicFuture[T](val self: Future[T]) {
-    def #>[U] (f: (T) => U)(implicit ec: ExecutionContext): Future[U] = {
-      self.map(f)
-    }
-
-    def ~>[U] (f: (T) => Unit)(implicit ec: ExecutionContext): Future[T] = {
-      self.foreach(f)
-      self
-    }
-
-    def #>>[U] (f: (T) => Future[U])(implicit ec: ExecutionContext): Future[U] = {
-      self.flatMap(f)
-    }
-
-    def >>[U] (f: Future[U])(implicit ec: ExecutionContext): Future[U] = {
-      self.flatMap(_ => f)
-    }
-
-    def #>>>[U] (f: Try[T] => Future[U])(implicit ec: ExecutionContext): Future[U] = {
-      self.flatMap { x =>
-        f(Success[T](x))
-      } recoverWith {
-        case e =>
-          try {
-            f(Failure(e))
-          } catch {
-            case e: Throwable => Future.failed[U](e)
-          }
-      }
-    }
-
-    def ?> (f: PartialFunction[Throwable, T])(implicit ec: ExecutionContext): Future[T] = {
-      self.recover(f)
-    }
-
-    def ?> (v: T)(implicit ec: ExecutionContext): Future[T] = {
-      self.recover(PartialFunction(_ => v))
-    }
-
-    def ?>>> (f: PartialFunction[Throwable, Future[T]])(implicit ec: ExecutionContext): Future[T] = {
-      self.recoverWith(f)
-    }
-
-    def ?>> (f: Future[T])(implicit ec: ExecutionContext): Future[T] = {
-      self.recoverWith(PartialFunction(_ => f))
-    }
-  }
-}
 
 object Futuristic extends App with MonadicFutures {
   import ExecutionContext.Implicits.global
@@ -78,11 +28,172 @@ object Futuristic extends App with MonadicFutures {
     4
   }
 
-  val res = f1 #> { x: Int =>
-    x * 2
-  } #>>> {
-    case Success(4) => f2
-  } ?>> f3 ~> println >> f1 ~> println
+  val res = f1 > { _ => 1 } >* { _ > 4 } <| println <* { _ => Some(20) } >< {
+    case Success(20) =>
+      println("moo")
+      f2
+  } <| println <>> { _ => f4 } <-> f3 <| println
 
-  Await.result(res, 1.seconds)
+  println(Await.result(res, 1.seconds))
+}
+
+trait MonadicFutures {
+
+  import scala.language.implicitConversions
+
+  implicit class MonadicFuture[T](self: Future[T]) {
+
+    /**
+      * Perform a side effecting operation on the successful result of this future
+      * @param f
+      * @param ec
+      * @return
+      */
+    def >|(f: (T) => Unit)(implicit ec: ExecutionContext): Future[T] = {
+      promiseOnComplete[T] { (t, p) =>
+        t foreach f
+        p complete t
+      }.future
+    }
+
+    /**
+      * Map successful value of this future.
+      * @param f
+      * @param ec
+      * @tparam U
+      * @return
+      */
+    def >[U](f: (T) => U)(implicit ec: ExecutionContext): Future[U] = {
+      self.map(f)
+    }
+
+    /**
+      * Filter (turn into failure)
+      * @param f
+      * @param ec
+      * @return
+      */
+    def >*(f: (T) => Boolean)(implicit ec: ExecutionContext): Future[T] = {
+      self.filter(f)
+    }
+
+    /**
+      * Flat Map successfull value
+      * @param f
+      * @param ec
+      * @tparam U
+      * @return
+      */
+    def >>[U](f: (T) => Future[U])(implicit ec: ExecutionContext): Future[U] = {
+      self.flatMap(f)
+    }
+
+    /**
+      * Flat Map the complete result, success and failure
+      * @param f
+      * @param ec
+      * @tparam U
+      * @return
+      */
+    def ><[U](f: Try[T] => Future[U])(implicit ec: ExecutionContext): Future[U] = {
+      promiseOnComplete[U] { (t, p) =>
+        f(t).onComplete(p.complete)
+      }.future
+    }
+
+    /**
+      * Join self to another future,
+      * Resolves only when both futures resolve, only return the result of the other future
+      *
+      * @param other
+      * @tparam U
+      * @return
+      */
+    def <->[U](other: Future[U])(implicit ec: ExecutionContext): Future[U] = {
+      promiseOnComplete[U] { (t, p) =>
+        other.onComplete(p.complete)
+      }.future
+    }
+
+    /**
+      * ForEach on failure
+      * @param f
+      * @param ec
+      * @return
+      */
+    def <|(f: (Throwable) => Unit)(implicit ec: ExecutionContext): Future[T] = {
+      promiseOnFailure {
+        case (e, p) => f(e); p.failure(e)
+      }.future
+    }
+
+    /**
+      * Filter on failure
+      * @param f
+      * @param ec
+      * @return
+      */
+    def <*(f: (Throwable) => Option[T])(implicit ec: ExecutionContext): Future[T] = {
+      promiseOnFailure { (e, p) =>
+        p.complete(Try({f(e).get}).orElse(Failure(e)))
+      }.future
+    }
+
+    /**
+      * Map on a failure
+      * @param f
+      * @param ec
+      * @tparam U
+      * @return
+      */
+    def <>[U <: Throwable](f: (Throwable) => U)(implicit ec: ExecutionContext): Future[T] = {
+      promiseOnFailure { (e, p) =>
+        p.failure(f(e))
+      }.future
+    }
+
+    /**
+      * Flat Map on failure
+      * @param f
+      * @param ec
+      * @return
+      */
+    def <>>(f: (Throwable) => Future[T])(implicit ec: ExecutionContext): Future[T] = {
+      promiseOnFailure { (e, p) => f(e).onComplete(p.complete) }.future
+    }
+
+    private def promiseOnSuccess[U](f: (T, Promise[U]) => Unit)
+                                         (implicit ec: ExecutionContext): Promise[U] = {
+      promiseOnComplete[U] { (t, p) =>
+        t match {
+          case Success(v) => f(v, p)
+          case Failure(e) => p.failure(e)
+        }
+      }
+    }
+
+    private def promiseOnFailure(f: (Throwable, Promise[T]) => Unit)
+                                      (implicit ec: ExecutionContext): Promise[T] = {
+      promiseOnComplete[T] { (t, p) =>
+        t match {
+          case Success(v) => p.success(v)
+          case Failure(e) => f(e, p)
+        }
+      }
+    }
+
+    private def promiseOnComplete[U](f: (Try[T], Promise[U]) => Unit)
+                                          (implicit ec: ExecutionContext): Promise[U] = {
+      val p = Promise[U]
+      self.onComplete { v =>
+        try {
+          f(v, p)
+        } catch {
+          case NonFatal(e) => p.failure(e)
+        }
+      }
+      p
+    }
+  }
+
 }
